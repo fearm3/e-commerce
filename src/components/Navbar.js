b3f558b50ad0023365d6b21259f4c7abac168d28
fearm3 <ibraaa1628@gmail.com>
2022-01-17 18:20:22 +0300

    add reducer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,7 @@ import { useProductsContext } from "../context/products_context";
 import { useUserContext } from "../context/user_context";
 
 const Nav = () => {
+  const { openSidebar } = useProductsContext();
   return (
     <NavContainer>
       <div className="nav-center">
@@ -16,12 +17,12 @@ const Nav = () => {
           <Link to="/">
             <img src={logo} alt="comfy sloth" />
           </Link>
-          <button type="button" className="nav-toggle">
+          <button type="button" className="nav-toggle" onClick={openSidebar}>
             <FaBars />
           </button>
         </div>
         <ul className="nav-links">
-          {links.map((link) => {
+          {links?.map((link) => {
             const { id, text, url } = link;
             return (
               <li key={id}>
