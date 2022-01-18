0ac57fa2753f783e2be89d1de94896ccb3d682dd
fearm3 <ibraaa1628@gmail.com>
2022-01-18 22:17:23 +0300

    add products page

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -16,6 +16,24 @@ const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_CLOSE) {
     return { ...state, isSidebarOpen: false };
   }
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true };
+  }
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    const featured_products = action.payload.filter(
+      (product) => product.featured === true
+    ); //filtered products_featured
+
+    return {
+      ...state,
+      products_loading: false, //spinner
+      products: action.payload,
+      featured_products,
+    };
+  }
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true };
+  }
 
   throw new Error(`No Matching "${action.type}" - action type`);
 };
