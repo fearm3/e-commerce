48f8bcb29254b65ce1d8e3cc2d72f43f19fd3cdd
fearm3 <ibraaa1628@gmail.com>
2022-01-22 17:14:23 +0300

    add context reducer

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -11,9 +11,24 @@ const AddToCart = ({ product }) => {
   const [mainColor, setMainColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
 
-  const increase = () => {};
-
-  const decrease = () => {};
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount;
+    });
+  };
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount;
+    });
+  };
 
   return (
     <Wrapper>
@@ -37,7 +52,11 @@ const AddToCart = ({ product }) => {
         </div>
       </div>
       <div className="btn-container">
-        <AmountButtons />
+        <AmountButtons
+          amount={amount}
+          increase={increase}
+          decrease={decrease}
+        />
         <Link to="/cart" className="btn">
           add to cart
         </Link>
