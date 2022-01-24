5d369128acec0b92e49834cb41e2de524f3674d9
fearm3 <ibraaa1628@gmail.com>
2022-01-24 15:01:39 +0300

    add localStorage

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -7,10 +7,18 @@ import {
   CLEAR_CART,
   COUNT_CART_TOTALS,
 } from "../actions";
-import { GiStarGate } from "react-icons/gi";
+
+const getLocalStorage = () => {
+  let cart = localStorage.getItem("cart");
+  if (cart) {
+    return JSON.parse(localStorage.getItem("cart"));
+  } else {
+    return [];
+  }
+};
 
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 534,
@@ -25,9 +33,20 @@ export const CartProvider = ({ children }) => {
   const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   };
+  //remove item
+  const removeItem = (id) => {};
+  //toggle amount
+  const toggleAmount = (id, value) => {};
+  //clear cart
+  const clearCart = () => {};
 
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
   return (
-    <CartContext.Provider value={{ ...state, addToCart }}>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
+    >
       {children}
     </CartContext.Provider>
   );
