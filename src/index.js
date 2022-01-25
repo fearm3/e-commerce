dbcac6e7269fdbfdf7f8127432ec339aa686cd4f
fearm3 <ibraaa1628@gmail.com>
2022-01-25 13:47:28 +0300

    add authentication

--- src/index.js
+++ src/index.js
@@ -8,14 +8,26 @@ import { CartProvider } from "./context/cart_context";
 import { UserProvider } from "./context/user_context";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+//react-e-commerce.eu.auth0.com
+//z4S6Hp28tdonNTqf6M0s8BpQ0BD906bX
+
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="react-e-commerce.eu.auth0.com"
+    clientId="z4S6Hp28tdonNTqf6M0s8BpQ0BD906bX"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>,
 
   document.getElementById("root")
 );
